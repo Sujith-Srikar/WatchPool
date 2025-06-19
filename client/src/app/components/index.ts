edd6f016744ea10515d6830cb3ed11e04c234a7a
Sujith Srikar <kandregulasujith@gmail.com>
2025-06-19 06:10:41 +0530

    Convert client from submodule to regular directory

---
+++ client/src/app/components/index.ts
@@ -0,0 +1,7 @@
+// index.js (or index.ts)
+export { default as Hero } from "./Hero";
+export { default as CTA } from "./CTA";
+export { default as Footer } from "./Footer";
+export { default as Feature } from "./Feature";
+export { Appbar } from "./Appbar";
+export { default as StreamView } from "./StreamView";
