f9b11a63578a9a14be2c1ef1b6bce90c71df5711
Sujith Srikar <kandregulasujith@gmail.com>
2025-06-19 06:03:31 +0530

    fix2

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ["i.ytimg.com"], // Add any other domains you need here
-  },
-};
-
-export default nextConfig;
--- src/app/components/index.ts
+++
@@ -1,7 +0,0 @@
-// index.js (or index.ts)
-export { default as Hero } from "./Hero";
-export { default as CTA } from "./CTA";
-export { default as Footer } from "./Footer";
-export { default as Feature } from "./Feature";
-export { Appbar } from "./Appbar";
-export { default as StreamView } from "./StreamView";
