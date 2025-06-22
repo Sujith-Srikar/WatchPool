009d8212c434a6e0fe9327975c6a8707c3bbfdec
Sujith Srikar <kandregulasujith@gmail.com>
2025-06-22 19:29:42 +0530

    Smooth Scroll Added

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,8 +1,21 @@
 "use client";
 import { Hero, CTA, Footer, Feature, Appbar } from "@/app/components/index";
 // import { authOptions } from "@/lib/auth-options";
+import Lenis from "lenis";
+import { useEffect } from "react";
 
 export default function LandingPage() {
+  
+  useEffect (()=>{
+    const lenis = new Lenis();
+
+    function raf(time: number): void {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+  })
   return (
     <>
       <div className="flex min-h-screen flex-col bg-stone-950 bg-page-gradient">
